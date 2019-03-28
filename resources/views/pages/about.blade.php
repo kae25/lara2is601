2bea09bf7ede4930807ceee152bb4135d5c00bec
Kayla Evans <kae25@njit.edu>
2019-03-28 19:46:14 -0400

    About Page Update

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
 
-        <h1>This is my about page!</h1>
+        <h1>About</h1>
         <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
 
 
