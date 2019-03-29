01384fa9b267da7a4748a84d83cb66610d9b6d76
Kayla Evans <kae25@njit.edu>
2019-03-29 18:20:04 -0400

    api.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -28,5 +28,12 @@ class PagesController extends Controller
         return view('pages.thankyou')->with(compact('name'));
     }
 
+    function show(Request $request) {
+        return $request->user();
+    }
+
 
 }
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,18 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:api')->get('/user', 'PagesController@show')->name('show');
+
+
+
+    //function (Request $request) {
+    //return $request->user();
+//});
+
+//Route::get('/', function () {
+//return view('pages.home');}
+
+
+//Route::get('/', 'PagesController@home')->name('home');
+
+//Route::get('/user', 'PagesController@show')->name('show');
